6665dd5e2cac1c662b0b4bec7d0708f0b707dd08
Haggai Dziesietnik <hdziesietnik@gmail.com>
2015-10-31 14:03:16 -0700

    add amazon s3 for paperclip uploads to heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,9 @@ class Pin < ActiveRecord::Base
 
 	belongs_to :user
 	
-	has_attached_file :image, :styles => { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, 
+	:styles => { medium: "300x300>", thumb: "100x100>" }
+	:bucket => '******',
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
